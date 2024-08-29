d322c623c4ebdc4f687567d755c52e8dbb9ed391
WillBark <will.e.barker@icloud.com>
2024-08-29 18:05:00 +0100

    Add files via upload

---
+++ Paper_4/GSE207209.R
@@ -0,0 +1,89 @@
+#Load Libraries
+library(dplyr)
+library(tidyverse)
+library(GEOquery)
+library(readxl)
+library(edgeR)
+
+#Read in Data
+data <- read.table('GSE207209_sample_id_ReadsperGene.txt', header = TRUE, sep = '\t')
+head(data)
+dim(data)
+
+#Get Metadata
+gse <- getGEO(GEO = 'GSE207209', GSEMatrix = TRUE)
+gse
+
+metadata <- pData(phenoData(gse[[1]]))
+head(metadata)
+
+#Metadata pipeline
+metadata_modified  <-  metadata %>%
+  select(1, 11, 12) %>% #Select required columns
+  #Rename columsn for ease of use
+  rename(ID = title) %>%
+  rename(tissue = characteristics_ch1.1) %>%
+  rename(race = characteristics_ch1.2) %>%
+  #Remove prefixes
+  mutate(tissue = gsub('tissue type: ', '', tissue)) %>%
+  mutate(race = gsub('race: ', '', race))
+
+#Reshaping data from wide to long
+data_long  <-  data %>%
+  gather(key = 'samples', value = 'FPKM', -gene_id)
+
+#Clean sample names to match metadata IDs
+data_long$samples <- gsub("Myo_", "MyoF_", data_long$samples)
+colnames(data)[2:ncol(data)] <- gsub("Myo_", "MyoF_", colnames(data)[2:ncol(data)])
+
+#Joining data_long and metadata_modified
+data_long <- data_long %>%
+  left_join(metadata_modified, by = c("samples" = "ID"))
+
+#Explore data
+
+#Make data a matrix
+matrix_data <- as.matrix(data[, 2:ncol(data)])
+rownames(matrix_data)<-data$gene_id
+
+#Filtering Dirty Data
+gene_means <- rowSums(matrix_data) #Mean for each gene
+
+overall_mean <- mean(gene_means) #Overall mean across all genes
+
+threshold <- overall_mean * 0.05 #Threshold as 5% of the overall mean
+
+filtered_matrix <- matrix_data[gene_means > threshold, ]
+dim(filtered_matrix)
+
+#Log2 Normalize Data
+DGE <- DGEList(counts = filtered_matrix) #DGEList object
+
+DGE <- calcNormFactors(DGE, method = "TMM") #Calculate normalization factors using TMM
+
+log2_normalized_matrix <- cpm(DGE, log = TRUE) #Normalize counts and log2 transform
+
+#Visual comparison
+par(mfrow = c(1, 2))
+boxplot(filtered_matrix, main = "Before Normalization", las = 2)
+boxplot(log2_normalized_matrix,
+        main = 'Boxplot of Samples',
+        ylab = 'Log2 Gene Number')
+
+#prepping metadata and data for PCA -> run the code CBF_PCA_Paper4.R
+
+metadata_modified_sel<-metadata_modified[metadata_modified$ID%in%colnames(log2_normalized_matrix),]
+metadata_modified_sel$homoGroup<-metadata_modified_sel$tissue  
+gsub(pattern = "Myometrium","Myometrium",metadata_modified_sel$homoGroup)  
+gsub(pattern = "Fibroid","Fibroid",metadata_modified_sel$homoGroup)
+
+#check that metadata is in the same order than the data
+metadata_modified_sel<-metadata_modified_sel[order(metadata_modified_sel$ID),]
+log2_normalized_matrix<-log2_normalized_matrix[,metadata_modified_sel$ID]
+colnames(log2_normalized_matrix)==metadata_modified_sel$ID
+
+CBF_PCA(data = t(log2_normalized_matrix),groups = metadata_modified_sel$homoGroup,scale = F,legendName = "group")
+
+#Saving log2 data as data frame
+log2_normalised_data <- as.data.frame(log2_normalized_matrix)
+write.csv(log2_normalised_data, 'log2_paper4', row.names = TRUE)
