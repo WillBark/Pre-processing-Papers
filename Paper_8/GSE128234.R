0e99a1ffc95ed2eda21a06bac39d74ac5a3b1bca
WillBark <will.e.barker@icloud.com>
2024-08-29 18:09:06 +0100

    Add files via upload

---
+++ Paper_8/GSE128234.R
@@ -0,0 +1,91 @@
+#Load Libraries
+library(dplyr)
+library(tidyverse)
+library(GEOquery)
+library(readxl)
+library(edgeR)
+
+
+#Read in Data
+data <- read.csv("matrix_paper8.csv", header = TRUE, sep = ",")
+head(data)
+dim(data)
+
+data[is.na(data)] <- 0 #Replacing NA with 0 so can be used in analysis
+names(data)[1] <- 'GeneID'
+
+#Get Metadata
+gse <- getGEO(GEO = 'GSE128234', GSEMatrix = TRUE)
+gse
+
+metadata <- pData(phenoData(gse[[1]]))
+head(metadata)
+
+#Metadata pipeline
+metadata_modified  <-  metadata %>%
+  select(1, 8, 10) %>% #Selecting required columns
+  #Renaming for ease of use
+  rename(ID = title) %>%
+  rename(source = source_name_ch1) %>%
+  rename(tissue = characteristics_ch1) %>%
+  #Removing prefix
+  mutate(tissue = gsub("tissue type: ", "", tissue))
+
+#So that metadata IDs match column names in data need to remove prefix
+metadata_modified$ID <- gsub(" \\[capture Hi-C\\]", "", metadata_modified$ID)
+metadata_modified$ID <- gsub("-", "_", metadata_modified$ID)
+
+#Reshaping data from wide to long
+data_long  <-  data %>%
+  gather(key = 'samples', value = 'FPKM', -GeneID)
+
+#Joining data_long and metadata_modified on samples and ID columns
+data_long <- data_long %>%
+  left_join(metadata_modified, by = c("samples" = "ID"))
+
+#Explore data
+
+#Make data a matrix
+matrix_data <- as.matrix(data[, 2:ncol(data)])
+rownames(matrix_data)<-data$GeneID
+
+#Filtering Dirty Data
+gene_means <- rowSums(matrix_data) #Mean for each gene
+
+overall_mean <- mean(gene_means) #Overall mean across all genes
+
+threshold <- overall_mean * 0.05 #Threshold as 5% of the overall mean
+
+filtered_matrix <- matrix_data[gene_means > threshold, ]
+dim(filtered_matrix)
+
+#Log2 Normalize Data
+DGE <- DGEList(counts = filtered_matrix) #DGEList object
+
+
+DGE <- calcNormFactors(DGE, method = "TMM") #Calculate normalization factors using TMM
+
+log2_normalized_matrix <- cpm(DGE, log = TRUE) #Normalize counts and log2 transform
+
+# Visual comparison
+par(mfrow = c(1, 2))
+boxplot(filtered_matrix, main = "Before Normalization", las = 2)
+boxplot(log2_normalized_matrix,
+        main = 'Boxplot of Samples',
+        ylab = 'Log2 Gene Number')
+
+#prepping metadata and data for PCA -> run the code CBF_PCA_Paper8.R
+
+metadata_modified_sel<-metadata_modified[metadata_modified$ID%in%colnames(log2_normalized_matrix),]
+metadata_modified_sel$homoGroup<-metadata_modified_sel$tissue  
+
+#check that metadata is in the same order than the data
+metadata_modified_sel<-metadata_modified_sel[order(metadata_modified_sel$ID),]
+log2_normalized_matrix<-log2_normalized_matrix[,metadata_modified_sel$ID]
+colnames(log2_normalized_matrix)==metadata_modified_sel$ID
+
+CBF_PCA(data = t(log2_normalized_matrix),groups = metadata_modified_sel$homoGroup,scale = F,legendName = "group")
+
+#Saving log2 data as data frame
+log2_normalised_data <- as.data.frame(log2_normalized_matrix)
+write.csv(log2_normalised_data, 'log2_paper8', row.names = TRUE)
