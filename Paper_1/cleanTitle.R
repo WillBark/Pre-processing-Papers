fa8ddd31d7980d1b7a603f2d2858e7bf5d7ed0ae
WillBark <will.e.barker@icloud.com>
2024-09-15 16:55:32 +0100

    Update cleanTitle.R

--- Paper_1/cleanTitle.R
+++ Paper_1/cleanTitle.R
@@ -1,13 +1,13 @@
 #Clean sample code to remove the prefix and [] from the title in metadata
-cleansample <- function(metadata_modified) {
-  finalvec <- rep(0, nrow(metadata_modified))
+cleansample <- function(metadata_modified) { #metadata_modified is the selection of specific columns from metadata
+  finalvec <- rep(0, nrow(metadata_modified)) #finalvec stores cleaned version of samples
   
   for (i in 1:nrow(metadata_modified)) {
     sample <- metadata_modified$sample[i]
     
     if (grepl('\\[.*\\]', sample)) {
       #Remove prefix and []
-      sample <- gsub('.*\\[|\\]', '', sample)
+      sample <- gsub('.*\\[|\\]', '', sample) #removing everything inbetween the [] for formatting
     }
     
     finalvec[i] <- sample
